080f2768dd1496b530c650721a55bd64566cb2c9
Raman Gupta <gr.gupta.raman@gmail.com>
2025-12-26 07:06:05 -0500

    feat: get link from db

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,17 +1,13 @@
+import { getLink } from '@repo/data-ops/queries/links';
 import { Hono } from 'hono';
 import { Bindings } from 'hono/types';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
-	const cf = c.req.raw.cf;
+	const id = c.req.param('id');
 
-	const country = cf?.country;
-	const lat = cf?.latitude;
-	const long = cf?.longitude;
-	return c.json({
-		country,
-		lat,
-		long,
-	});
+	const linkInfo = getLink(id);
+
+	return c.json(linkInfo);
 });
