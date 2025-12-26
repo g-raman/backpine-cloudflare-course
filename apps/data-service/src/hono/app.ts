6b128ea0d27f063ec344c0a88882397a81c82cc7
Raman Gupta <gr.gupta.raman@gmail.com>
2025-12-25 21:22:45 -0500

    feat: get country, lat, and long from request

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -4,7 +4,14 @@ import { Bindings } from 'hono/types';
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
+	const cf = c.req.raw.cf;
+
+	const country = cf?.country;
+	const lat = cf?.latitude;
+	const long = cf?.longitude;
 	return c.json({
-		message: 'Hello World!',
+		country,
+		lat,
+		long,
 	});
 });
