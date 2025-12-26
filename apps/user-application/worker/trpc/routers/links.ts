a24d66f4c4e1b02fb59081a65731e1fc68ca35a9
Raman Gupta <gr.gupta.raman@gmail.com>
2025-12-25 20:58:52 -0500

    fix: add missing queries

--- apps/user-application/worker/trpc/routers/links.ts
+++ apps/user-application/worker/trpc/routers/links.ts
@@ -13,6 +13,7 @@ import {
 } from "@repo/data-ops/queries/links";
 
 import { TRPCError } from "@trpc/server";
+import { ACTIVE_LINKS_LAST_HOUR, LAST_30_DAYS_BY_COUNTRY } from "./dummy-data";
 
 export const linksTrpcRoutes = t.router({
   linkList: t.procedure
@@ -66,4 +67,23 @@ export const linksTrpcRoutes = t.router({
     .mutation(async ({ input }) => {
       await updateLinkDestinations(input.linkId, input.destinations);
     }),
+  activeLinks: t.procedure.query(async () => {
+    return ACTIVE_LINKS_LAST_HOUR;
+  }),
+  totalLinkClickLastHour: t.procedure.query(async () => {
+    return 13;
+  }),
+  last24HourClicks: t.procedure.query(async () => {
+    return {
+      last24Hours: 56,
+      previous24Hours: 532,
+      percentChange: 12,
+    };
+  }),
+  last30DaysClicks: t.procedure.query(async () => {
+    return 78;
+  }),
+  clicksByCountry: t.procedure.query(async () => {
+    return LAST_30_DAYS_BY_COUNTRY;
+  }),
 });
