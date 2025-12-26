430a2631bfc15f01a141c4975f16652f1a36cc54
Raman Gupta <gr.gupta.raman@gmail.com>
2025-12-26 09:53:46 -0500

    feat: send data to queue

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,5 +1,6 @@
 import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
-import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { cloudflareInfoSchema, LinkSchemaType } from '@repo/data-ops/zod-schema/links';
+import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
 import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
@@ -19,6 +20,21 @@ App.get('/:id', async (c) => {
 
 	const headers = cfHeader.data;
 	const destination = getDestinationForCountry(linkInfo, headers.country);
+
+	const queueMessage: LinkClickMessageType = {
+		type: 'LINK_CLICK',
+		data: {
+			id: id,
+			country: headers.country,
+			destination: destination,
+			accountId: linkInfo.accountId,
+			latitude: headers.latitude,
+			longitude: headers.longitude,
+			timestamp: new Date().toISOString(),
+		},
+	};
+
+	c.executionCtx.waitUntil(c.env.QUEUE.send(queueMessage));
 
 	return c.redirect(destination);
 });
